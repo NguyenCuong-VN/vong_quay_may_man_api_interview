8b5949cf255a126f29892c328acaed8483c67ea9
cuong.nguyen <cuong.nguyen@quant-edge.com>
2022-08-02 22:33:38 +0700

    add log

--- controller/item-controller.js
+++ controller/item-controller.js
@@ -5,7 +5,9 @@ const ItemController = {
   //get list item
   getAll: async (req, res) => {
     try {
+      console.log('Go in getAll ItemController');
       const items = await findItem({}, true);
+      console.log('Go out getAll ItemController');
       res.status(200).json(items);
     } catch (error) {
       res.status(500).json(error);
